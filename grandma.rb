b5aa844ae9896361358d03eb8b81cd7c69934427
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-06 07:27:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,7 +19,7 @@ def speak_to_grandma(phrase)
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
 end
-end
+
 
     # phrase == phrase.upcase
     # return "NO, NOT SINCE 1938!"
