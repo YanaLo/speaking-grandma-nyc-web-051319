4421816cb307a8f0b3b66c378346e1ccc9bc6c9e
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-06 07:27:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,13 +14,13 @@
 def speak_to_grandma(phrase)
   if phrase!=phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
-    if shout == shout.upcase()
+     elsif shout == shout.upcase()
     return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
 end
 end
-end
+
     # phrase == phrase.upcase
     # return "NO, NOT SINCE 1938!"
 
